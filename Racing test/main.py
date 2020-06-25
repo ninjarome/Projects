4d31560f4b8eb0eae7d94d972d97e4485be91919
ninjarome <66064666+ninjarome@users.noreply.github.com>
2020-06-25 22:39:14 +1200

    Learning the basics

---
+++ Racing test/main.py
@@ -0,0 +1,20 @@
+import pygame
+
+pygame.init()
+gameDisplay = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Racing test')
+clock = pygame.time.Clock()
+
+crashed = False
+
+while not crashed:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            crashed = True
+
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
+quit()
