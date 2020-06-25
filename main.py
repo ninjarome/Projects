03d60a0a1cca040c925f6aa76df7ab0b6c3a847a
ninjarome <66064666+ninjarome@users.noreply.github.com>
2020-06-25 21:19:38 +1200

    moving to folder

--- main.py
+++
@@ -1,8 +0,0 @@
-import pygame, sys
-
-pygame.init()
-
-gameDisplay = pygame.display.set_mode((500, 500))
-pygame.display.set_caption('Sweeper of Bombs')
-clock = pygame.time.Clock()
-crashed = False
