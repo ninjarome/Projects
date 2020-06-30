a63d0f574e8ce994a3f052f6ffb357c8010b5966
ninjarome <66064666+ninjarome@users.noreply.github.com>
2020-06-30 21:30:17 +1200

    Added car image that moves left and right

--- Racing test/main.py
+++ Racing test/main.py
@@ -1,9 +1,28 @@
 import pygame
 
+display_width = 800
+display_height = 600
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+
+carImg = pygame.image.load('car.png')
+
 pygame.init()
-gameDisplay = pygame.display.set_mode((800, 600))
+gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Racing test')
 clock = pygame.time.Clock()
+
+def car(x, y):
+    gameDisplay.blit(carImg, (x, y))
+
+x = (display_width * 0.45)
+y = (display_height * 0.8)
+
+x_change = 0
 
 crashed = False
 
@@ -12,7 +31,21 @@ while not crashed:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crashed = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x_change = -5
+            elif event.key == pygame.K_RIGHT:
+                x_change = 5
+        
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                x_change = 0
 
+
+    x += x_change
+            
+    gameDisplay.fill(white)        
+    car(x, y)
     pygame.display.update()
     clock.tick(60)
 
