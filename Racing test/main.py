e24b1c3edb1a8560ced816d44da7985161af53ce
ninjarome <66064666+ninjarome@users.noreply.github.com>
2020-07-02 21:24:18 +1200

    created scoring system and completed first iteration of game

--- Racing test/main.py
+++ Racing test/main.py
@@ -21,6 +21,11 @@ gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Racing test')
 clock = pygame.time.Clock()
 
+def things_dodged(count):
+    font = pygame.font.SysFont(None, 25)
+    text = font.render("Dodged: " + str(count), True, black)
+    gameDisplay.blit(text, (5, 0))
+
 def things(thingx, thingy, thingw, thingh, color):
     pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
 
@@ -60,6 +65,8 @@ def game_loop():
     thing_width = 100
     thing_height = 100
 
+    dodged = 0
+
     gameExit = False
 
     while not gameExit:
@@ -87,11 +94,12 @@ def game_loop():
         pygame.draw.rect(gameDisplay, black, [display_width - 5 , 0 , 5,  display_height])
         pygame.draw.rect(gameDisplay, black, [0, 0 , 5,  display_height])
         
+        
         #things(thingx, thingy, thingw, thingh, color):
         things(thing_startx, thing_starty, thing_width, thing_height, black)
         thing_starty += thing_speed
         car(x, y)
-        
+        things_dodged(dodged)
 
         #car crossing boundries and crashing
         if x + car_width > display_width - 5 or x < 5:
@@ -101,6 +109,8 @@ def game_loop():
         if thing_starty > display_height:
             thing_starty = 0 - thing_height
             thing_startx = random.randrange(0, display_width)
+            dodged += 1
+            thing_speed += 1
 
         #if the car crashes into thing
         if y < thing_starty + thing_height and y + car_height > thing_starty:
