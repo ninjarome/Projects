5c06c264dac0f9da1604a7ff063723f3e848fb96
ninjarome <66064666+ninjarome@users.noreply.github.com>
2020-07-28 21:52:26 +1200

    Added mines data

--- Minesweeper/main.py
+++ Minesweeper/main.py
@@ -12,23 +12,24 @@ green = (0, 255, 0)
 blue = (0, 0, 255)
 
 class button():
-    def __init__(self, color, x,y,width,height, count):
+    def __init__(self, color, outline, x,y,width,height, count):
         self.color = color
+        self.outline = outline
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.count = count
+        self.outline = outline
 
-    def draw(self,win,outline=None):
+    def draw(self,win):
         #Call this method to draw the button on the screen
-        if outline:
-            pygame.draw.rect(win, outline, (self.x,self.y,self.width,self.height),0)
+        if self.outline:
+            pygame.draw.rect(win, self.outline, (self.x,self.y,self.width,self.height),0)
             pygame.draw.rect(win, self.color, (self.x+3,self.y+3,self.width-7,self.height-7),0)
 
         else:
             pygame.draw.rect(win, self.color, (self.x,self.y,self.width,self.height),0)
-            
         
 
     def isOver(self, pos):
@@ -39,15 +40,40 @@ class button():
             
         return False
 
+def placeMines():
+    mineList = []
+    mines = False
+    for i in range (0, 50):
+        m = random.randrange(0, 15)
+        if m < 2:
+            mineList.append(1)
+        else:
+            mineList.append(0)
+    for i in mineList:
+        if i == 1:
+            mines = True
+    while not mines:
+        mineList = placeMines
+    return mineList
+
 pygame.init()
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Sweeper of Bombs')
 clock = pygame.time.Clock()
 
-
 def game_loop():
     gameExit = False
+    butt = button(green, blue, 0, 100, 50, 50, 0)
+    mines = placeMines()
     while not gameExit:
+        gameDisplay.fill(black)
+        pygame.draw.rect(gameDisplay, white, [0 , 100 , display_width,  display_height])
+
+        butt.draw(gameDisplay)
+        
+        pygame.display.update()
+        clock.tick(120)
+        
         pos = pygame.mouse.get_pos()
 
         for event in pygame.event.get():
@@ -57,15 +83,15 @@ def game_loop():
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if butt.isOver(pos):
-                    print("Pressed")
+                    print(len(mines))
 
-        gameDisplay.fill(black)
-        pygame.draw.rect(gameDisplay, white, [0 , 100 , display_width,  display_height])
+            if event.type == pygame.MOUSEMOTION:
+                if butt.isOver(pos):
+                    butt.color = blue
+                else:
+                    butt.color = green
 
-        butt = button((green), 0, 100, 50, 50, 0)
-        butt.draw(gameDisplay, blue)
-        pygame.display.update()
-        clock.tick(120)
+
 
 game_loop()
 pygame.quit()
