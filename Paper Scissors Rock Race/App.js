bde55f36cdffffce82de6b35d45bc242b38f59ea
ninjarome <66064666+ninjarome@users.noreply.github.com>
2021-10-11 16:48:09 +1300

    Got timer to work properly
    Timer now disappears when button is pressed and starts when appears

--- Paper Scissors Rock Race/App.js
+++ Paper Scissors Rock Race/App.js
@@ -1,9 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TouchableHighlight, Image } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TouchableHighlight, Image, Button } from 'react-native';
 import CountDown from 'react-native-countdown-component';
 
 export default function App() {
+const [start, setStart] = useState(false)
+const [counter, setCounter] = useState()
+const [p1word, setp1word] = useState('Player 1')
+const [p2word, setp2word] = useState('Player 2')
+const [playWord, setPlayWord] = useState('Play')
+
+
+function change(){
+  if(!start){
+    setStart(true)
+    setPlayWord('Cancel')
+  }else if (start){
+    setStart(false)
+    setPlayWord('Play Again')
+  }
+}
+
   return (
     <View style={styles.container}>
       <View style={[styles.container, {alignItems: "flex-end"}, {flexDirection: "row"},{
@@ -19,15 +36,32 @@ export default function App() {
       <Image style={styles.picture} source={require('./assets/Rock.png')} />
     </TouchableHighlight>
       </View>
-      <View style={[styles.container, {backgroundColor: 'rgba(0,0,0,0.1)'}, {flexDirection: 'row'}]}>
 
-    <CountDown 
-      until={3}
-      onPress={() => alert('hi')}
-      size={20}
-      timeToShow={['S']}
-      />
+      {start? (
+      <View style={[styles.container, {width: '100%'}]}>
+        <View style={[{transform: [{ rotate: '180deg' }]}]}>
+          <CountDown
+          until={3}
+          size={20}
+          timeToShow={['S']}
+          />
+        </View>
+
+        <Button title={playWord} onPress={() => change()}/>
+
+        <CountDown 
+        until={3}
+        size={20}
+        timeToShow={['S']}
+        />
+
       </View>
+      ) : (
+        <View style={[styles.container, {width: '100%'}]}>
+      <Button title={playWord} onPress={() => change()}/>
+      </View>
+      )}
+
       <View style={[styles.container, {alignItems: "flex-end"}, {flexDirection: "row"}]}>
       <TouchableHighlight>
       <Image style={styles.picture} source={require('./assets/Paper.png')} />
