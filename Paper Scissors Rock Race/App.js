268dcf548070f45ee9991106d963e592d1552101
ninjarome <66064666+ninjarome@users.noreply.github.com>
2021-10-14 15:56:01 +1300

    Basically finished

--- Paper Scissors Rock Race/App.js
+++ Paper Scissors Rock Race/App.js
@@ -9,9 +9,13 @@ const [counter, setCounter] = useState()
 const [p1word, setp1word] = useState('Player 1')
 const [p2word, setp2word] = useState('Player 2')
 const [playWord, setPlayWord] = useState('Play')
-const [p1, setp1] = useState('')
-const [p2, setp2] = useState('')
-const [winner, setWinner] = useState('...')
+const [p1, setp1] = useState('--------')
+const [p2, setp2] = useState('--------')
+const [p1score, setP1score] = useState(0)
+const [p2score, setP2score] = useState(0)
+const [p1colour, setP1colour] = useState('rgba(0,0,0,0.1)')
+const [p2colour, setP2colour] = useState('rgba(0,0,0,0.1)')
+
 
 function change(){
   if(!start){
@@ -21,6 +25,8 @@ function change(){
     setp1word('Player 1')
     setp2word('Player 2')
     setPlayWord('Cancel')
+    setP1colour('rgba(0,0,0,0.1)')
+    setP2colour('rgba(0,0,0,0.1)')
   }else if (start){
     setStart(false)
     setPlayWord('Play Again')
@@ -28,27 +34,51 @@ function change(){
 }
 
 function wincalc(){
-  if(p1 == p2){
-    setWinner(' TIE')
+  setPlayWord('Reset')
+  if(p1 == '' && p2 != ''){
+    setp1word("You didn't choose in time\nYou Lose")
+    setp2word("They didn't choose in time\nYou Win!")
+    setP1colour('#FF6161')
+    setP2colour('#B0FF61')
+    setP2score(p2score + 1)
+  }
+  else if(p2 == '' && p1 != ''){
+    setp2word("You didn't choose in time\nYou Lose")
+    setp1word("They didn't choose in time\nYou Win!")
+    setP1colour('#B0FF61')
+    setP2colour('#FF6161')
+    setP1score(p1score + 1)
+  }
+  else if(p1 == p2){
+    setp1word('TIE!')
+    setp2word('TIE!')
+    setP2colour('#FFFF61')
+    setP1colour('#FFFF61')
   }
   else if(p1 == 'Paper' && p2 == 'Rock'
   || p1 == 'Scissors' && p2 == 'Paper'
   || p1 == 'Rock' && p2 == 'Scissors'){
-    setWinner(' Player 1!')
+    setp1word('You Win!')
+    setp2word('You Lose')
+    setP1colour('#B0FF61')
+    setP2colour('#FF6161')
     setP1score(p1score + 1)
-  }else if(p2 == 'Paper' && p1 == 'Rock'
+  }
+  else if(p2 == 'Paper' && p1 == 'Rock'
   || p2 == 'Scissors' && p1 == 'Paper'
   || p2 == 'Rock' && p1 == 'Scissors'){
-    setWinner(' Player 2!')
+    setp2word('You Win!')
+    setp1word('You Lose')
+    setP1colour('#FF6161')
+    setP2colour('#B0FF61')
     setP2score(p2score + 1)
   }
 }
 
-
   return (
     <View style={styles.container}>
       {p2 == '' ? (
-        <View style={[styles.container, {alignItems: "flex-end"}, {flexDirection: "row"},
+        <View style={[styles.container, {width: '100%'}, {alignItems: "flex-end"}, {flexDirection: "row"},
         {transform: [{ rotate: '180deg' }]},  {backgroundColor: 'rgba(0,0,0,0.1)'}, {flex: 0.45}
         ]}>
         <TouchableOpacity onPress={() => {setp2('Paper'); setp2word('Player 2 picked')}}>
@@ -62,54 +92,83 @@ function wincalc(){
         </TouchableOpacity>
         </View>) : (
 
-        <View style={[styles.container, {alignItems: "flex-end"}, {flexDirection: "row"},
+        <View style={[styles.container, {width: '100%'}, {alignItems: "flex-end"}, {flexDirection: "row"},
         {transform: [{ rotate: '180deg' }]},  {backgroundColor: 'rgba(0,0,0,0.1)'}, {flex: 0.45}, {opacity: 0.5}
         ]}>
-          <Image style={styles.picture} source={require('./assets/Paper.png')}/>
-          <Image style={styles.picture} source={require('./assets/Scissors.png')}/>
-          <Image style={styles.picture} source={require('./assets/Rock.png')}/>
+          {p2 == 'Paper'? (<Image style={[styles.picture, {backgroundColor: '#00DBFF'}]} source={require('./assets/Paper.png')}/>):
+          (<Image style={styles.picture} source={require('./assets/Paper.png')}/>)}
+          {p2 == 'Scissors'? (<Image style={[styles.picture, {backgroundColor: '#FF2727'}]} source={require('./assets/Scissors.png')}/>):
+          (<Image style={styles.picture} source={require('./assets/Scissors.png')}/>)}
+          {p2 == 'Rock'? (<Image style={[styles.picture, {backgroundColor: '#2ECE00'}]} source={require('./assets/Rock.png')}/>):
+          (<Image style={styles.picture} source={require('./assets/Rock.png')}/>)}
         </View>
       )}
 
-      <Text style={[styles.title, {justifyContent: "flex-start"}, {
+      <Text style={[styles.title, {justifyContent: "flex-start"}, {width: '100%'},{backgroundColor: p2colour}, {
         transform: [{ rotate: '180deg' }]}]}>{p2word}{'\n'}{p2}</Text>
 
       {start? (
       <View style={[styles.container, {width: '100%'}]}>
-        <View style={[{transform: [{ rotate: '180deg' }]}]}>
+        <View style={[{transform: [{ rotate: '180deg' }]}, {width: '100%'}]}>
+          <Text style={[styles.title, {fontSize: 20}, {alignSelf: 'flex-start'}, {position: 'absolute'}]}>P2(You): {p2score}</Text>
+          <CountDown
+          until={3}
+          size={20}
+          timeToShow={['S']}
+          onFinish={() => wincalc()}
+          />
+          <Text style={[styles.title, {fontSize: 20}, {alignSelf: 'flex-end'}, {position: 'absolute'}]}>P1: {p1score}</Text>
+        </View>
+
+        <Button title={playWord} onPress={() => change()}/>
+        <View style={{width: '100%'}}>
+          <Text style={[styles.title, {fontSize: 20}, {alignSelf: 'flex-start'}, {position: 'absolute'}]}>P1(You): {p1score}</Text>
           <CountDown
           until={3}
           size={20}
           timeToShow={['S']}
           />
+          <Text style={[styles.title, {fontSize: 20}, {alignSelf: 'flex-end'}, {position: 'absolute'}]}>P2: {p2score}</Text>
         </View>
-
-        <Button title={playWord} onPress={() => change()}/>
-
-        <CountDown 
-        until={3}
-        size={20}
-        timeToShow={['S']}
-        />
-        </View>
+      </View>
       ) : (
         <View style={[styles.container, {width: '100%'}]}>
-      <Button title={playWord} onPress={() => change()}/>
+          <View style={[{width: '100%'}, {transform: [{ rotate: '180deg' }]}]}>
+            <Text style={[styles.title, {fontSize: 20}, {alignSelf: 'flex-start'}, {position: 'absolute'}]}>P2(You): {p2score}</Text>
+            <Text style={[styles.title, {fontSize: 20}, {alignSelf: 'flex-end'}, {position: 'absolute'}]}>P1: {p1score}</Text>
+          </View>
+          <Button title={playWord} onPress={() => change()}/>
+          <View style={{width: '100%'}}>
+            <Text style={[styles.title, {fontSize: 20}, {alignSelf: 'flex-start'}, {position: 'absolute'}]}>P1(You): {p1score}</Text>
+            <Text style={[styles.title, {fontSize: 20}, {alignSelf: 'flex-end'}, {position: 'absolute'}]}>P2: {p2score}</Text>
+          </View>
       </View>
       )}
 
-      <Text style={styles.title}>{p1word}{'\n'}{p2}</Text>
-      <View style={[styles.container, {alignItems: "flex-end"}, {flexDirection: "row"}, {flex: 0.45}]}>
-        <TouchableOpacity onPress={() => {setp1('Paper'); setp1word('Player 1 picked')}}>
-          <Image style={styles.picture} source={require('./assets/Paper.png')}/>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => {setp1('Scissors'); setp1word('Player 1 picked')}}>
-          <Image style={styles.picture} source={require('./assets/Scissors.png')}/>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => {setp1('Rock'); setp1word('Player 1 picked')}}>
-          <Image style={styles.picture} source={require('./assets/Rock.png')}/>
-        </TouchableOpacity>
-      </View>
+      <Text style={[styles.title, {width: '100%'}, {backgroundColor: p1colour}]}>{p1word}{'\n'}{p1}</Text>
+      {p1 == ''? (
+        <View style={[styles.container, {width: '100%'}, {alignItems: "flex-end"}, {flexDirection: "row"}, {flex: 0.45}]}>
+          <TouchableOpacity onPress={() => {setp1('Paper'); setp1word('Player 1 picked')}}>
+            <Image style={styles.picture} source={require('./assets/Paper.png')}/>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {setp1('Scissors'); setp1word('Player 1 picked')}}>
+            <Image style={styles.picture} source={require('./assets/Scissors.png')}/>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {setp1('Rock'); setp1word('Player 1 picked')}}>
+            <Image style={styles.picture} source={require('./assets/Rock.png')}/>
+          </TouchableOpacity>
+        </View>) : (
+        <View style={[styles.container, {width: '100%'}, {alignItems: "flex-end"}, {flexDirection: "row"},
+        {backgroundColor: 'rgba(0,0,0,0.1)'}, {flex: 0.45}, {opacity: 0.5}
+        ]}>
+          {p1 == 'Paper'? (<Image style={[styles.picture, {backgroundColor: '#00DBFF'}]} source={require('./assets/Paper.png')}/>):
+          (<Image style={styles.picture} source={require('./assets/Paper.png')}/>)}
+          {p1 == 'Scissors'? (<Image style={[styles.picture, {backgroundColor: '#FF2727'}]} source={require('./assets/Scissors.png')}/>):
+          (<Image style={styles.picture} source={require('./assets/Scissors.png')}/>)}
+          {p1 == 'Rock'? (<Image style={[styles.picture, {backgroundColor: '#2ECE00'}]} source={require('./assets/Rock.png')}/>):
+          (<Image style={styles.picture} source={require('./assets/Rock.png')}/>)}
+        </View>)}
+
       <StatusBar style="auto" />
     </View>
   );
@@ -131,7 +190,8 @@ const styles = StyleSheet.create({
     width: 100
   },
   title:{
-    fontSize: 30,
+    textAlign: "center",
+    fontSize: 20,
     fontWeight: 'bold',
   }
 });
